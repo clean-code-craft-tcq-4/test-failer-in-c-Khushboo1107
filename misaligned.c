457526cadc667e69cdd37c100533a9acf28ba9a7
Khushboo1107 <105203869+Khushboo1107@users.noreply.github.com>
2022-06-29 00:12:01 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,22 +1,30 @@
 #include <stdio.h>
 #include <assert.h>
+int getColorNumber(int i, int j){
+return i*5+j;
+}
 
+char* formatRow(int colorNumber, const char* majorColorName, const char* minorColorName){
+    static char rowBuffer[80];
+    sprintf(rowBuffer, "%d | %s | %s\n", colorNumber, majorColorName, minorColorName);
+}
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            printf(formatRow(getColorNumber(i,j), majorColor[i], minorColor[i]));
         }
     }
     return i * j;
 }
 
 int main() {
+    assert(getColorNumber(0,0)==1;
+    
     int result = printColorMap();
     assert(result == 25);
-    assert(result == 24);
     printf("All is well (maybe!)\n");
     return 0;
 }
